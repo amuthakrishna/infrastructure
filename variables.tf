d11a625ee984672868fec4a0927210e33b8ed544
Krishnamoorthy <krishnamoorthy@pinnacleseven.com>
2025-07-26 09:28:39 +0530

    rails-app

--- variables.tf
+++ variables.tf
@@ -66,17 +66,17 @@ variable "env_s3_key" {
   default     = "rails_app.env"
 }
 
-variable "container_image_app" {
-  description = "ECR image URI for Rails app"
-  type        = string
-#  default     = "413745378153.dkr.ecr.ap-south-1.amazonaws.com/rails-app-rails-app:latest"
-}
+# variable "container_image_app" {
+#   description = "ECR image URI for Rails app"
+#   type        = string
+# #  default     = "413745378153.dkr.ecr.ap-south-1.amazonaws.com/rails-app-rails-app:latest"
+# }
 
-variable "container_image_nginx" {
-  description = "ECR image URI for Nginx"
-  type        = string
-#  default     = "413745378153.dkr.ecr.ap-south-1.amazonaws.com/rails-app-nginx:v2"
-}
+# variable "container_image_nginx" {
+#   description = "ECR image URI for Nginx"
+#   type        = string
+# #  default     = "413745378153.dkr.ecr.ap-south-1.amazonaws.com/rails-app-nginx:v2"
+# }
 
 variable "environment" {
   description = "Environment name (dev, staging, prod)"
